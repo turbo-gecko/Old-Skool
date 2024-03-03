e448760b2d2fc7e352de11081eff306c00ab0442
turbo-gecko <55830240+turbo-gecko@users.noreply.github.com>
2024-03-03 20:53:13 +1300

    Change to moon.asm
    TASM friendly build of moon.

--- TEC-1G/Games/lcd.asm
+++ TEC-1G/Games/lcd.asm
@@ -4,26 +4,43 @@
 ; 2nd March 2024
 ;---------------------------------------------------------------------
 
+;---------------------------------------------------------------------
+; The LCD address for colomun 1 on each row of the LCD
+;---------------------------------------------------------------------
 LCD_1:      .equ	80h
 LCD_2:      .equ	0c0h
 LCD_3:      .equ	94h
 LCD_4:      .equ	0d4h
 
 ;---------------------------------------------------------------------
+; Sends the clear command to the LCD
 ;
+; Inputs:
+;   None
+; Updates:
+;   None
+; Destroys:
+;   A
 ;---------------------------------------------------------------------
-CLEAR_LCD:  rst		28h             ;  Wait for LCD to not be busy before...
+CLEAR_LCD:  rst		28h             ; Wait for LCD to not be busy before...
 
-            ld		a,01h           ;  ...sending the LCD clear command and...
+            ld		a,01h           ; ...sending the LCD clear command and...
             out		(04),a
-            rst		28H             ;  ...wait for it to complete
+            rst		28H             ; ...wait for it to complete
 
             ret
 
 ;---------------------------------------------------------------------
+; Displays a 16 bit integer in 5 digit format 00000 on the LCD
 ;
+; Inputs:
+;   HL  - Register containing the 16 bit integer
+; Updates:
+;   None
+; Destroys:
+;   A, BC, HL
 ;---------------------------------------------------------------------
-HL_TO_ASC:  ld		bc,-10000       ;  Convert the 16 bit value in HL to it's ASCII equivalent
+HL_TO_LCD:  ld		bc,-10000       ; Convert the 16 bit value in HL to it's ASCII equivalent
             call	HTA_1
             ld		bc,-1000
             call	HTA_1
@@ -38,14 +55,21 @@ HTA_2:      inc		a
             add		hl,bc
             jr		c,HTA_2
             sbc		hl,bc
-            ld		c,14            ;  Send ASCII character to the LCD
+            ld		c,14            ; Send ASCII character to the LCD
             rst		10h
             ret
 
 ;---------------------------------------------------------------------
+; Displays an 8 bit integer in 3 digit format 000 on the LCD
 ;
+; Inputs:
+;   L   - Register containing the 8 bit integer
+; Updates:
+;   None
+; Destroys:
+;   A, BC, HL
 ;---------------------------------------------------------------------
-L_TO_ASC:   ld		h,0             ;  Convert the 8 bit value in L to it's ASCII equivalent
+L_TO_ASC:   ld		h,0             ; Convert the 8 bit value in L to it's ASCII equivalent
             ld		bc,-100
             call	LTA_1
             ld		c,-10
@@ -58,7 +82,7 @@ LTA_2:      inc		a
             add		hl,bc
             jr		c,LTA_2
             sbc		hl,bc
-            ld		c,14            ;  Send ASCII character to the LCD
+            ld		c,14            ; Send ASCII character to the LCD
             rst		10h
             ret
 
--- TEC-1G/Games/serial.asm
+++ TEC-1G/Games/serial.asm
@@ -7,17 +7,24 @@
 ;---------------------------------------------------------------------
 ; Variables
 ;---------------------------------------------------------------------
-S_TEMP_D:   .dw		0
+S_TEMP_D:   .db		"000",0
 S_TEMP_W:   .db		"00000",0
 
 ;---------------------------------------------------------------------
-; Send a single character out the serial port
+; Send a hex character representation of a byte out the serial port
+;
+; Inputs:
+;   A   - Contains the byte to TX
+; Updates:
+;   S_TEMP_D    - Temporary variable for holding 2 
+; Destroys:
+;   A, C, HL
 ;---------------------------------------------------------------------
-SER_BYTE:   ld		de,S_TEMP_D     ;  'a' should already be loaded with the byte to tx
+SER_BYTE:   ld		de,S_TEMP_D     ; 'a' should already be loaded with the byte to tx
             ld		c,09
             rst		10h
 
-            ld		hl,(S_TEMP_D)   ;  Send out the serial port
+            ld		hl,(S_TEMP_D)   ; Send out the serial port
             ld		a,(S_TEMP_D)
             ld		c,22
             rst		10h
@@ -30,6 +37,13 @@ SER_BYTE:   ld		de,S_TEMP_D     ;  'a' should already be loaded with the byte to
 
 ;---------------------------------------------------------------------
 ; Send a CR/LF pair out the serial port
+;
+; Inputs:
+;   None
+; Updates:
+;   None
+; Destroys:
+;   A, C
 ;---------------------------------------------------------------------
 SER_CRLF:   ld		a,13
             ld		c,22
@@ -43,6 +57,13 @@ SER_CRLF:   ld		a,13
 
 ;---------------------------------------------------------------------
 ; Send a string pointed to by HL out the serial port
+;
+; Inputs:
+;   HL  - Contains the pointer to the string to TX
+; Updates:
+;   None
+; Destroys:
+;   A, C, HL
 ;---------------------------------------------------------------------
 SER_STR:    ld		a,(hl)
             cp		0
@@ -57,8 +78,15 @@ SS_1:       ret
 
 ;---------------------------------------------------------------------
 ; Send a formatted 16 bit value out the serial port
+;
+; Inputs:
+;   HL   - Contains the character to tx
+; Updates:
+;   None
+; Destroys:
+;   A, C, DE, HL
 ;---------------------------------------------------------------------
-SER_WORD:   ld		de,S_TEMP_W     ;  hl should already be loaded with the word to tx
+SER_WORD:   ld		de,S_TEMP_W     ; hl should already be loaded with the word to tx
             ld		c,08
             rst		10h
             ld		a,0
@@ -70,10 +98,17 @@ SER_WORD:   ld		de,S_TEMP_W     ;  hl should already be loaded with the word to 
             ret
 
 ;---------------------------------------------------------------------
+; Convert the 16 bit value in BC to it's ASCII equivalent
 ;
+; Inputs:
+;   BC   - Contains the 16 bit value to convert
+; Updates:
+;   None
+; Destroys:
+;   A, BC, DE, HL
 ;---------------------------------------------------------------------
-SER_HL2D:   ld		de,S_TEMP_W     ;  Convert the 16 bit value in HL to it's ASCII equivalent
-            ld		bc,-10000       ;  for use in serial comms
+SER_HL2D:   ld		de,S_TEMP_W
+            ld		bc,-10000
             call	H2D_1
             ld		bc,-1000
             call	H2D_1
